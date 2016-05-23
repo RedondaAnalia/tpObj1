9dc98624170971b20399fd7288a2f81cac4a75b2
@RedondaAnalia <a.redonda89@gmail.com>
2016-05-23 18:44:40 -0300

    Primer commit

---
+++ tpObj2-Concesionaria/src/tpC/Concesionaria.java
@@ -0,0 +1,16 @@
+package tpC;
+
+import java.util.ArrayList;
+
+public class Concesionaria {
+
+	private Fabrica fabrica;
+	private ArrayList<Cliente> clientes;
+	
+	public Concesionaria(Fabrica fab){
+		this.fabrica=fab;
+		this.clientes= new ArrayList<Cliente>;
+	}
+	
+}
+
